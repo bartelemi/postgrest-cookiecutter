e55f8b18cc9d592c586b96ad361d6972bc8c832a
Bartlomiej Szostek <szostek.bartek@gmail.com>
2021-09-18 14:39:55 +0100

    feat: rename openapi url in the hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,7 @@ import json
 import os
 import sys
 import tarfile
+from contextlib import suppress
 from pathlib import Path
 from shutil import move
 from urllib.request import urlopen, urlretrieve
@@ -28,27 +29,47 @@ def fetch_release_bundle(tarball_url: str):
         sys.exit(-2)
 
 
-def unpack_release(file_name):
+def unpack_release(file_name: str, destination: str):
+    unpacked_folder = Path("")
     try:
         with tarfile.open(file_name) as tar:
+            unpacked_object = None
             for info in tar.getmembers():
                 if "/dist/" in info.name and "flavors" not in info.name:
                     tar.extract(info)
-                    unpack_location = info.name
-            move(Path(unpack_location).parent, Path("www/swagger"))
+                    unpacked_object = info.name
+            unpacked_folder = Path(unpacked_object).parent
+            move(unpacked_folder, Path(destination))
     except Exception as error:
         print("Failed to unpack release bundle:", error)
         sys.exit(-3)
     finally:
-        os.remove(file_name)
+        with suppress(OSError):
+            os.remove(file_name)
+        with suppress(OSError):
+            os.remove(unpacked_folder.parent)
+
+
+def set_openapi_doc_url(root: Path):
+    index_html = root / "index.html"
+    sample_openapi_url = "https://petstore.swagger.io/v2/swagger.json"
+    project_openapi_url = "http://api:3000/swagger.json"
+    with open(index_html) as original:
+        updated_index_html = original.read().replace(
+            sample_openapi_url, project_openapi_url
+        )
+    with open(index_html, "w") as updated:
+        updated.write(updated_index_html)
 
 
 def main():
     github_repo = "swagger-api/swagger-ui"
+    destination = Path("www/swagger")
     release_info = get_release_info(github_repo)
     file_name = fetch_release_bundle(release_info["tarball_url"])
-    unpack_release(file_name)
+    unpack_release(file_name, destination)
+    set_openapi_doc_url(destination)
 
 
 if "{{ cookiecutter.use_swagger_ui }}" == "y":
-    main()    
+    main()
